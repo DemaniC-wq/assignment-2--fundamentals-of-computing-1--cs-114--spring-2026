e0662fa4f2b012222b2bff0a1daa4536f9014cca
DemaniC-wq <demclarke@hartford.edu>
2026-03-13 16:45:35 -0400

    Added Scanner
    This will take user input since we need to read the users input as a string

--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
 public class PrintVowelsAndNonVowels {
   public static void main(String[] args) {
+
+  Scanner scan = new Scanner(System.in);
+
+  System.out.println("Enter a string:");
+  String text= scan.nextLine();
+
   }
 }
