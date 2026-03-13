7aff470674cb0c3beae7170ffb16eadb27a42129
DemaniC-wq <demclarke@hartford.edu>
2026-03-13 16:46:15 -0400

    Completed Program

--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -13,5 +13,28 @@ public class PrintVowelsAndNonVowels {
   int ocount= 0;
   int ucount= 0;
   int nonVowelCount= 0;
+
+  for(int i = 0; i < text.length(); i++){
+  char c = text.charAt(i);
+  if (c == 'a' || c == 'A') {
+  acount++;
+  }else if (c == 'e' || c == 'E') {
+  ecount++;
+  } else if (c == 'i' || c == 'I') {
+  icount++;
+  }else if (c == 'o' || c == 'O') {
+  ocount++;
+  } else if (c == 'u' || c == 'U') {
+  ucount++;
+  }else {
+  nonVowelCount++;
+  }
+  System.out.println("a" + acount);
+  System.out.println("e" + ecount);
+  System.out.println("i" + icount);
+  System.out.println("o" + ocount);
+  System.out.println("u" + ucount);
+  System.out.println("Non-vowels: " + nonVowelCount);
+
   }
 }
