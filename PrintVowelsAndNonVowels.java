d5803328d53400303bbebb4ee14230cad598daa1
DemaniC-wq <demclarke@hartford.edu>
2026-03-13 16:46:04 -0400

    declared and initialized variables

--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -7,5 +7,11 @@ public class PrintVowelsAndNonVowels {
   System.out.println("Enter a string:");
   String text= scan.nextLine();
 
+  int acount= 0;
+  int ecount= 0;
+  int icount= 0;
+  int ocount= 0;
+  int ucount= 0;
+  int nonVowelCount= 0;
   }
 }
