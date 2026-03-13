3c5dfa5d402868d930a14b4ce2e094bc067724c3
DemaniC-wq <demclarke@hartford.edu>
2026-03-13 16:03:16 -0400

    Added a while loop
    This while loop is used to ensure that the user inputs a valid number of verses

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -8,5 +8,9 @@
 
   System.out.println("Enter the number of verses to print  (1-100):");
   verses = scan.nextInt();
+
+  while(verses<1 || verses>100){
+  System.out.println("Please enter a number between 1 and 100:");
+  verses = scan.nextInt();
   }
 }
