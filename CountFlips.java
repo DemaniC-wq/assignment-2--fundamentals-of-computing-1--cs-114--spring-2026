2c2f81530e08cd8df1b7bd136cac7fe16d1265c9
DemaniC-wq <demclarke@hartford.edu>
2026-03-13 14:54:03 -0400

    Added a for loop and myCoin.flip
    The for loop will run 100 times and the myCoin.flip will simulate the flipping of the coin.

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,10 @@
 public class CountFlips {
   public static void main(String[] args) {
+  Coin myCoin = new Coin();
+
+  int headsCount = 0;
+  int tailsCount = 0;
+  for(int i = 1;i <=100;i++){
+  myCoin.flip();
   }
 }
