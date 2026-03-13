bc08e7e76124c849b7b802aa0aa2dc62b04c8ebb
DemaniC-wq <demclarke@hartford.edu>
2026-03-13 15:44:05 -0400

    Added print statements
    I used string concatenation to display the number of times it landed on heads and tails.

--- CountFlips.java
+++ CountFlips.java
@@ -1,10 +1,24 @@
-public class CountFlips {
+
+
+  public class CountFlips {
   public static void main(String[] args) {
   Coin myCoin = new Coin();
 
   int headsCount = 0;
   int tailsCount = 0;
+
+
   for(int i = 1;i <=100;i++){
   myCoin.flip();
+
+  if (myCoin.isHeads()) {
+  headsCount++;}
+  else{
+  tailsCount++;
   }
-}
+  }
+  System.out.println("Number of heads: " + headsCount);
+  System.out.println("Number of tails: " + tailsCount);
+  }
+
+  }
