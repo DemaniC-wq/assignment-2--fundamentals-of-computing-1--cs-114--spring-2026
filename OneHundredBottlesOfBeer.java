3fcca0fe66db07939d05b775bb192a73d6a8399d
DemaniC-wq <demclarke@hartford.edu>
2026-03-13 15:56:24 -0400

    Added Scanner
    I added a scanner to receive user input and i declared the variable "verses"

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,4 +1,12 @@
-public class OneHundredBottlesOfBeer {
+  import java.util.Scanner;
+
+  public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+
+  int verses;
+
+  System.out.println("Enter the number of verses to print  (1-100):");
+  verses = scan.nextInt();
   }
 }
